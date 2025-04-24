e41ca4876c15cddb66225bf52100edfa15c90ab9
Taru Wiik <taruwiik@gmail.com>
2025-04-24 13:25:18 +0300

    Added features to show a single book note to index.ejs. Updated partials because I did not find where I could checkout auto tag endings from code and did not want to use time for that

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
+// Import modules for the app to use
 import express from "express";
+import bodyParser from "body-parser";
 
+// Initialize the Express app and set the port for the server to listen to
 const app = express();
 const port = 3000;
 
+// Middlewear to parse incoming request bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+// Makes the contents of the "public" directory accessible to the client
+app.use(express.static("public"));
+
+// Main page route
 app.get("/", (req, res) => {
-  res.render("Log connection");
+  res.render("index.ejs");
 });
 
+// Start the server
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
