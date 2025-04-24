d0dfa9b55de903fb10a8279168d0db8ae1e6839f
Taru Wiik <taruwiik@gmail.com>
2025-04-24 21:55:08 +0300

    Added db connection and fetching all the notes to be displayed for client

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // Import modules for the app to use
 import express from "express";
 import bodyParser from "body-parser";
-import { createdbClient } from "./dbConfig";
+import { createdbClient } from "./dbConfig.js";
 
 // Initialize the Express app and set the port for the server to listen to
 const app = express();
 const port = 3000;
 
+// Create a db client to Node.js with pg library
 const db = createdbClient();
 db.connect();
 
@@ -15,15 +16,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Makes the contents of the "public" directory accessible to the client
 app.use(express.static("public"));
 
-//test data for notes
-const notes = [
-  { title: "AA", note: "Aa" },
-  { title: "BB", note: "Bb" },
-  { title: "CC", note: "Cc" },
-];
+async function getAllNotesFromdb() {
+  try {
+    const result = await db.query("SELECT * FROM notes");
+    var notes = result.rows;
+
+    console.log(notes);
+  } catch (err) {
+    console.log(err);
+  }
+  return notes;
+}
 
 // Main page route
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+  const notes = await getAllNotesFromdb();
   res.render("index.ejs", {
     bookNotes: notes,
   });
